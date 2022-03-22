b4fbe4009e74f4bc664045f17f69fd41ac4af718
Bhattacharyya-Avi <ab.kodeeo@gmail.com>
2022-03-22 16:48:36 +0600

    added some string method

--- basic/string/index.php
+++ basic/string/index.php
@@ -40,26 +40,41 @@ echo"add \ before t"."<br>";
 echo addcslashes($text,"t");
 echo "<br>";
 
-echo"addslashes()";
+echo"addslashes()"."<br>";
+echo"add backslashes before and end of double quotation"."<br>";
 $text2 = 'i love "my" country.';
 // add backslashes before and end of "" word. 
 echo addslashes($text2);
 echo "<br>";
 
-echo "bin2hex()";
+echo "bin2hex() convert text in it's binary form";
 // convert any text in its binary form.
 echo bin2hex($text);
 echo "<br>";
 
-echo "chop()";
+echo "chop() remove text from the right side of the original text."."<br>";
 // remove  selected text from the right side of the original text.
-echo chop($text,'industry.');
+echo chop($text,'industry.')."<br>";
+echo "removed industry.";
 echo "<br>";
 
-echo "chr()";
+echo "chr() convet ascii to character"."<br>";
 // converts any ascii value in to character 
 echo "character of ascii value  53"."<br>";
 echo chr(053)." in decimal <br>"; // for decimal value
 echo chr(053)." in octal<br>"; // Octal value
 echo chr(0x53)." in hex"; // hex value
 echo "<br>";
+
+echo "chunk_split() splits a string into a series of smaller parts.". "<br>";
+echo chunk_split($text,5,".")."<br>";
+
+echo "convert_uuencode() convert string into encoded form"."<br>";
+echo convert_uuencode($text)."<br>";
+
+echo "convert_uudecode() convert encoded text into normal text"."<br>";
+$str = ",2&5L;&\@=V]R;&0A `";
+echo " example text-> Hello world! "."<br>";
+echo convert_uudecode($str)."<br>";
+
+
